434fcde1cabbbed5829ec38a7ba7f74292832fbd
Umang <namangya.pcs@gmail.com>
2026-07-04 23:48:43 +0530

    Python Day1 Part1

---
+++ Python/Udemy - 100 Days of Code (Complete Python Bootcamp)/Day1.py
@@ -0,0 +1,31 @@
+# 1st July 2026 - 4th July 2026
+print("Day 1 ->> 1st July 2026 - 4th July 2026")
+print("\n")
+
+# First Program
+print("Hello World!")
+print("\n")
+
+# Use \n to add another line in the statement
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.\n3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
+print("\n")
+
+# Use + for concatinating the strings
+print("Without" + "Space")
+print("With" + " " + "Space")
+print("\n")
+
+# Code From Debugging Exercise Python interprets:
+# \n → newline
+# \t → tab
+# \" → double quote
+# \\ → literal backslash
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \\n")
+
+# Input Function
+print("User is" + " " + input("What is your age ?") + " " + "year old.")
