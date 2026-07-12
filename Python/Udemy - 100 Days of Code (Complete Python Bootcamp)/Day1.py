f133d47d8fc34e843f6485f73754fca4a8fd8610
Umang <namangya.pcs@gmail.com>
2026-07-13 01:05:24 +0530

    Complete Day1 and Start Day2

--- Python/Udemy - 100 Days of Code (Complete Python Bootcamp)/Day1.py
+++ Python/Udemy - 100 Days of Code (Complete Python Bootcamp)/Day1.py
@@ -1,4 +1,5 @@
-# 1st July 2026 - 4th July 2026
+# 1st July 2026 - 12th July 2026 Basic and Variables
+
 print("Day 1 ->> 1st July 2026 - 4th July 2026")
 print("\n")
 
@@ -28,4 +29,24 @@ print("String Concatenation is done with the + sign")
 print("New lines can be created with a \\n")
 
 # Input Function
-print("User is" + " " + input("What is your age ?") + " " + "year old.")
+print("User is" + " " + input("What is your age ?\n") + " " + "year old.")
+
+
+# Use of Variable
+variable=input("What is your Name:\n")
+
+#Use of Length
+length=len(variable)
+
+print("My Length of Name is",length)
+
+
+# Final Project of Band Name Generator
+print("\nWelcome to the Band Name Generator\n")
+city=input("What's the name of the City you grew up in\n")
+pet=input("What's your pet name\n")
+print("Your Band Name could be" + " " + city + " " + pet)
+print("Your Band Name could be",city,pet)
+
+
+# End of Day 1
--- Python/Udemy - 100 Days of Code (Complete Python Bootcamp)/Day2.py
+++ Python/Udemy - 100 Days of Code (Complete Python Bootcamp)/Day2.py
@@ -1,1 +1,3 @@
+# 13th July 2026 - 14th July 2026 - Working with Data and Manipulate Strings
+
 print("Day 2")
