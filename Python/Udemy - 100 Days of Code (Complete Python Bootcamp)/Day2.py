ef627f115128099f18d499816ae54d0056b74bf6
Umang <namangya.pcs@gmail.com>
2026-07-14 00:44:15 +0530

    Python Day2 partially complete

--- Python/Udemy - 100 Days of Code (Complete Python Bootcamp)/Day2.py
+++ Python/Udemy - 100 Days of Code (Complete Python Bootcamp)/Day2.py
@@ -1,3 +1,25 @@
 # 13th July 2026 - 14th July 2026 - Working with Data and Manipulate Strings
 
-print("Day 2")
+# Specific Character of the String
+print("Day 2"[2])
+print("Day 2"[-1])
+
+print(123+456)
+
+# To make large number readable we use underscore in it.
+print(12_34_56_789)
+
+# String, Byte, Tuple, List, Range, Collection, Set, Frozen Set
+
+# To Know the datatype of the given value
+print(type("What"))
+print(type(12345))
+print(type(123.45))
+print(type(False))
+
+# Casting DataTypes
+print("1" + "2")
+print(int("1")+int("2"))
+int(), str(), bool(), float()
+
+print("Number of Letter in your Name: " + str(len(input("What is your Name\n"))))
