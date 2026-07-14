907ee853694e84fb5219cfb920553422bfe02f4a
Umang <namangya.pcs@gmail.com>
2026-07-14 11:28:52 +0530

    End to Day2 and Start of Day3

--- Python/Udemy - 100 Days of Code (Complete Python Bootcamp)/Day2.py
+++ Python/Udemy - 100 Days of Code (Complete Python Bootcamp)/Day2.py
@@ -22,4 +22,54 @@ print("1" + "2")
 print(int("1")+int("2"))
 int(), str(), bool(), float()
 
-print("Number of Letter in your Name: " + str(len(input("What is your Name\n"))))
+# print("Number of Letter in your Name: " + str(len(input("What is your Name\n"))))
+
+# Mathematical Operation (P.E.M.D.A.S)
+
+print(123 + 3.45)
+print(2 - 1)
+print(3 * 5)
+print(12 / 2)
+print(12 // 2)
+print(2 ** 3)
+
+#Thonny to Debug Python Code
+print(325+37+656+125+2000+35+28+160+200+105)
+
+print(round(3.45678))
+print(round(3.75678))
+print(round(3.45678,3))
+
+score=0
+test=0
+
+score = score + 1
+test +=1
+
+print("Test = ",test," Score = ",score)
+
+score=23
+height=2.3
+really=True
+
+# F String with which we can use all Data type in the string
+print(f"Your Score is = {score}, with height = {height}. Is it really {really}")
+
+print("Welcome to Tip Calculator\n")
+total_bill=float(input("What was the total bill? "))
+tip_amount=float(input("How much tip would you like to give ? 10, 15, or 20 or any amount? "))
+people_split=float(input("How many people to split the bill? "))
+each_person= round(((total_bill + tip_amount) / people_split ),2)
+print("Each person should pay:",str(each_person))
+
+print("Welcome to Tip Calculator\n")
+total_bill=float(input("What was the total bill? "))
+tip_percentage=float(input("How much tip would you like to give ? 10, 15, or 20 or any percent? "))
+tip_amount=float((total_bill * tip_percentage) / 100)
+people_split=float(input("How many people to split the bill? "))
+each_person= round(((total_bill + tip_amount) / people_split ),2)
+print("Each person should pay:",str(each_person))
+print(f"Each person should pay: {each_person}")
+
+
+# End of Day 2
---
+++ Python/Udemy - 100 Days of Code (Complete Python Bootcamp)/Day3.py
@@ -0,0 +1,17 @@
+# 14th July 2026 - 15th July 2026 - Control Flow and Logical Operators
+
+# If Condition
+#     Do This
+# else
+#     Do This
+
+# In Python Spacing and Identation is Really Important
+
+print("Welcome to the If Else Game")
+height=int(input("What is your height in cm: "))
+
+if height > 168:
+    print("Your Height good for Selection")
+else:
+    less_height = 168 - height
+    print(f"Sorry, you height is {less_height} cm less from eligibity")
